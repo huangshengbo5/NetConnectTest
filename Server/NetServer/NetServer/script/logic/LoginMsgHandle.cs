8d49c23517065927b7b3bfdff70a2fdde4688a08
黄圣博 <huangshengbo@colorfulgame.cn>
2020-08-28 18:07:59 +0800

    update

--- Server/NetServer/NetServer/script/logic/LoginMsgHandle.cs
+++ Server/NetServer/NetServer/script/logic/LoginMsgHandle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetServer.script.logic;
 using NetServer.script.net;
 
 partial class MsgHandler
@@ -20,5 +21,59 @@ partial class MsgHandler
             msg.result = 1;
         }
         NetManager.Send(c,msg);
+    }
+
+    public static void MsgLogin(ClientState c, MsgBase msgBase)
+    {
+        MsgLogin msg = (MsgLogin)msgBase;
+        if (!DbManager.CheckPassword(msg.id, msg.pw))
+        {
+            msg.result = 1;
+            NetManager.Send(c,msg);
+            return;
+        }
+
+        if (c.player!=null)
+        {
+            msg.result = 1;
+            NetManager.Send(c,msg);
+            return;
+        }
+
+        if (PlayerManager.IsOnline(msg.id))
+        {
+            Player other = PlayerManager.GetPlayer(msg.id);
+            MsgKick msgKick = new MsgKick();
+            msg.result = 0;
+            other.Send(msgKick);
+            NetManager.Close(other.state);
+        }
+
+        //获取玩家数据
+        PlayerData playerData = DbManager.GetPlayerData(msg.id);
+        if (playerData == null)
+        {
+            msg.result = 1;
+            NetManager.Send(c,msg);
+            return;
+        }
+
+        Player player = new Player(c);
+        player.id = msg.id;
+        player.data = playerData;
+        PlayerManager.AddPlayer(msg.id,player);
+        c.player = player;
+        msg.result = 0;
+        player.Send(msg);
+    }
+
+    public static void OnDisconnect(ClientState c)
+    {
+        Console.WriteLine("Close");
+        if (c.player != null)
+        {
+            DbManager.UpdatePlayerData(c.player.id, c.player.data);
+            PlayerManager.RemovePlayer(c.player.id);
+        }
     }
 }
--- Server/NetServer/NetServer/script/logic/NotepadMsgHandle.cs
+++ Server/NetServer/NetServer/script/logic/NotepadMsgHandle.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetServer.script.logic;
+using NetServer.script.net;
 
-namespace NetServer.script.logic
+partial class MsgHandler
 {
-    class NotepadMsgHandle
+    public static void MsgGetText(ClientState c, MsgBase msgBase)
     {
+        MsgGetText msg = (MsgGetText) msgBase;
+        Player player = c.player;
+        if (player == null)
+        {
+            return;
+        }
+
+        msg.text = player.data.text;
+        player.Send(msg);
+    }
+
+    public static void MsgSaveText(ClientState c, MsgBase msgBase)
+    {
+        MsgSaveText msg = (MsgSaveText) msgBase;
+        Player player = c.player;
+        if (player == null)
+        {
+            return;
+        }
+        player.data.text = msg.text;
+        player.Send(msg);
     }
 }

6a2115fcac45f2d0e7e149e8d9a595312508c71a
黄圣博 <huangshengbo@colorfulgame.cn>
2020-08-29 12:58:09 +0800

    update

---
+++ Assets/Script/NetChatRoomTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetChatRoomTest : MonoBehaviour
+{
+    public InputField idInput;
+
+    public InputField pwInput;
+
+    public InputField textInput;
+
+
+    // Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

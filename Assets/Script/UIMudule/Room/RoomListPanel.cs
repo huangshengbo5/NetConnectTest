6a9f0f8a3dfe09bfc99ddfe1fe339b738df014cc
黄圣博 <huangshengbo@colorfulgame.cn>
2020-09-09 12:27:29 +0800

    完善UI框架

---
+++ Assets/Script/UIMudule/Room/RoomListPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomListPanel : BasePanel {
+
+    public override void OnInit()
+    {
+        skinPath = "RoomListPanel";
+        layer = Layer.Panel;
+    }
+
+    public override void OnShow(params object[] args)
+    {
+
+    }
+
+    public override void OnClose()
+    {
+
+    }
+}
---
+++ Assets/Script/UIMudule/Room/RoomPanel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomPanel : BasePanel {
+
+	//账号文本
+    private Text idText;
+
+    //战绩文本
+    private Text scoreText;
+    //创建房间按钮
+    private Button createButton;
+    //刷新列表按钮
+    private Button reflashButton;
+    //列表容器
+    private Transform content;
+    //房间物体
+    private GameObject roomObj;
+
+    public override void OnShow(params object[] para)
+    {
+        idText = skin.transform.Find("InfoPanel/IdText").GetComponent<Text>();
+        scoreText = skin.transform.Find("InfoPanel/scoreText").GetComponent<Text>();
+        createButton = skin.transform.Find("CtrlPanel/CreateButton").GetComponent<Button>();
+        reflashButton = skin.transform.Find("CtrlPanel/ReflashButton").GetComponent<Button>();
+
+        content = skin.transform.Find("ListPanel/Scroll View/Viewport/Content");
+        roomObj = skin.transform.Find("Room").gameObject;
+
+        //按钮事件
+        createButton.onClick.AddListener(OnCreateClick);
+        reflashButton.onClick.AddListener(OnReflashClick);
+        //不激活房间
+        roomObj.SetActive(false);
+        //显示id
+        idText.text = GameMain.id;
+
+        //协议监听
+        FrameWorkNetManager.AddMsgListener("MsgGetAchieve",OnMsgGetAchieve);
+
+    }
+
+    public void OnCreateClick()
+    {
+
+    }
+
+    public void OnReflashClick()
+    {
+
+    }
+
+    public void OnMsgGetAchieve(FrameWorkMsgBase str)
+    {
+
+    }
+}
